6060ddc401dd31d033c689462d40ebe4faa0d752
Basit Tramboo <basit.tramboo@gmail.com>
2025-05-20 18:57:56 -0300

    feat: add Resume component

---
+++ src/components/Resume.jsx
@@ -0,0 +1,22 @@
+function Resume() {
+  return (
+    <section id="resume">
+      <h2>Resume</h2>
+
+      <p>
+        You can download my resume using the link below.
+      </p>
+
+      <a
+        href="assets/Basit-Tramboo-Resume.pdf"
+        target="_blank"
+        rel="noreferrer"
+        className="resume-button"
+      >
+        📄 View Resume
+      </a>
+    </section>
+  );
+}
+
+export default Resume;

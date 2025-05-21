24a3bd9d3b97c51c36a35f21faec6acfc79e3c66
Basit Tramboo <basit.tramboo@gmail.com>
2025-05-21 09:17:45 -0300

    feat: update Education section content

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -7,7 +7,7 @@ function Education() {
       <div className="education-entry">
         <h3>
           Dalhousie University
-          <span>Sept 2024 – Jan 2026</span>
+          {/* <span>Sept 2024 – Jan 2026</span> */}
         </h3>
         <p>Master of Applied Computer Science – Halifax, NS</p>
         <ul>
@@ -18,9 +18,12 @@ function Education() {
       <div className="education-entry">
         <h3>
           Islamic University of Science and Technology
-          <span>Sept 2012 – Aug 2016</span>
+          {/* <span>Sept 2012 – Aug 2016</span> */}
         </h3>
         <p>Bachelor of Technology in Computer Science and Engineering – Kashmir, India</p>
+        <ul>
+          <li>Relevant Courses: DSA, Web Development, Mobile Systems</li>
+        </ul>
       </div>
     </section>
   );
